0a75e9a5d3d2f56223648de76b19e6990fb9b883
freeofzn <freeofzn@naver.com>
2024-10-26 16:29:09 +0900

    .

--- springboot-security-jwt-mybatis/src/main/resources/static/html/index.js
+++ springboot-security-jwt-mybatis/src/main/resources/static/html/index.js
@@ -6,15 +6,6 @@ const getBaseURL = () => {
   }
 };
 
-// 쿠키에서 특정 이름의 값을 가져오는 함수
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) {
-    return parts.pop().split(";").shift();
-  }
-}
-
 // 페이지 전환 함수
 function showPage(page) {
   // 모든 페이지 숨기기
